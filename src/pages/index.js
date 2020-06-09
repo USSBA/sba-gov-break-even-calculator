4602351fe9a1c1f2858ca8dd420dc63252a7b3b9
Parissa <parissa.eggleston@gmail.com>
2020-06-09 16:41:37 -0400

    add circle-ci config and gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Break Even Calculator</h1>
+    <p>Welcome to Break Even Calculator.</p>
+    <p>Now go build something great.</p>
+
+    <Link to="/page-2/">Go to page 2</Link> <br />
+  </Layout>
+)
+
+export default IndexPage

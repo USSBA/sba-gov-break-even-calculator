ff1ac780755cbaaa45b158e2b3800251e81a68a2
Parissa <parissa.eggleston@gmail.com>
2020-06-10 17:03:16 -0400

    test circleci

--- src/pages/index.test.js
+++ src/pages/index.test.js
@@ -5,6 +5,6 @@ import IndexPage from './index.js'
 describe('IndexPage', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<IndexPage />)
-    expect(wrapper).toHaveLength(1);
+    expect(wrapper).toHaveLength(5);
   })
 })

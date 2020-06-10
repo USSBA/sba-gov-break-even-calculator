4178036502adc604ba0457da83af407f4e20c70f
Parissa <parissa.eggleston@gmail.com>
2020-06-10 15:20:02 -0400

    config files for jest, lint and gatsby

---
+++ src/pages/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import IndexPage from './index.js'
+
+describe('IndexPage', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<IndexPage />)
+    expect(wrapper).toHaveLength(1);
+  })
+})

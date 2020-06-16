777eb216739e42fa5ec30db2a8a522578f2f2480
Parissa <parissa.eggleston@gmail.com>
2020-06-16 16:55:24 -0400

    code cleanup

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,25 +9,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import "semantic-ui-less/semantic.less";
 
-import { useStaticQuery, graphql } from "gatsby"
-import { Container} from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import Header from "./header"
 import Footer from "./footer"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header />
       <Container>
         <main>{children}</main>
       </Container>

146153dfb3e59a51d12fb04f30f179da94ea8679
Ksenia Coulter <kcoulter@Ksenias-MBP.cable.rcn.com>
2020-06-18 15:08:52 -0400

    Create gradient background and position calculator icon

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,3 +1,16 @@
 import React from 'react'
-import calculatorIcon from '../images/calculator_icon.svg'
+import { Image } from 'semantic-ui-react'
+import calculatorIcon from '../../images/calculator_icon.svg'
+import './hero.less'
 
+
+const Hero = (Props) => {
+  return(
+    <div className='heroContainer'>
+      <Image src={calculatorIcon} alt='calculator icon' size='small' centered/>
+      <div className='blueGradient'></div>
+    </div>
+  )
+}
+
+export default Hero

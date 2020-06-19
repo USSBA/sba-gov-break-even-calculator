7253e18ec84f06b03af173607b363a18dd97fbbd
Ksenia Coulter <kcoulter@gmail.com>
2020-06-19 16:22:52 -0400

    Make footer component more like the header component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,13 +4,15 @@ import logo from '../../images/sba_400x400.jpg'
 import { Image, Grid } from 'semantic-ui-react'
 
 const Footer = () => (
-  <Grid id='footer' className='footer' columns={1}>
-    <Grid.Column textAlign='center'>
-      <div className='footer-divider'></div>
-      <Image src={logo} size='tiny' alt='Small Business Administration' centered />
-      U.S. Small Business Administration | 409 3rd St, SW. Washington DC 20416
-    </Grid.Column>
-  </Grid>
+  <footer className='becFooter'>
+    <Grid className='footer' columns={1}>
+      <Grid.Column textAlign='center'>
+        <div className='footer-divider'></div>
+        <Image src={logo} size='tiny' alt='Small Business Administration' centered />
+        U.S. Small Business Administration | 409 3rd St, SW. Washington DC 20416
+      </Grid.Column>
+    </Grid>
+  </footer>
 )
 
 export default Footer

2b3026ac8dc3a54abec99b0c0233cb28d305e972
Ksenia Coulter <kcoulter@gmail.com>
2020-06-25 17:10:29 -0400

    Fix failing test

--- src/components/organisms/stepOne/stepOne.test.js
+++ src/components/organisms/stepOne/stepOne.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import StepOne from './stepOne'
-import { fixedCostInitState } from './helper'
+import { fixedCostInitState } from './stepOneFieldsData'
 
 describe('StepOne', () => {
   it('renders without crashing', () => {

0d942f74dd5a0e197c227624ad77e371610276d4
Ksenia Coulter <kcoulter@gmail.com>
2020-06-29 10:13:11 -0400

    Add test to step three

---
+++ src/components/organisms/stepThree/stepThree.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { Form } from 'semantic-ui-react'
+
+import { MoneyInput } from '../../atoms'
+// import './stepTwo.less'
+
+const StepThree = (props) => {
+  const [unitPrice, setUnitPrice] = useState(0)
+  const handleSubmit = () => {
+    props.setUnitPrice(unitPrice)
+    props.goToStep(4)
+  }
+  return (
+    <div className='stepTwo-container'>
+      <h3>Estimate your selling price per unit</h3>
+      <p>Calculate the price at which your unit or service will sell to customers.</p>
+      <Form onSubmit={handleSubmit}>
+        <div>
+          <label for='unit price'>Per unit sellign price*</label>
+          <p>Enter the price you plan to sell per unit or service</p>
+          <MoneyInput 
+            onChange={(e, {value}) => setUnitPrice(value)}
+            name='unit price' />
+        </div>
+        <div className='button-container'>
+          <Form.Button primary content='Continue' />
+        </div>
+        <a onClick={()=>{props.goToStep(1)}}>Restart Analysis</a>
+      </Form>
+    </div>
+  )
+}
+
+export default StepThree;
---
+++ src/components/organisms/stepThree/stepThree.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import StepThree from './StepThree'
+
+describe('StepThree', () => {
+  it('renders money input field and submit button', () => {
+    const wrapper = shallow(<StepThree />)
+    expect(wrapper.find('MoneyInput')).toHaveLength(1)
+    expect(wrapper.find('FormButton')).toHaveLength(1)
+  })
+
+  it('calls setNumUnits prop and goes to step 4 on submit', () => {
+    const setUnitPriceMock = jest.fn()
+    const goToStepMock = jest.fn()
+    const wrapper = shallow(
+      <StepThree setUnitPrice={setUnitPriceMock} goToStep={goToStepMock} />
+    )
+    wrapper.find('Form').simulate('submit')
+    expect(setUnitPriceMock).toHaveBeenCalledWith(0)
+    expect(goToStepMock).toHaveBeenCalledWith(4)
+  })
+})

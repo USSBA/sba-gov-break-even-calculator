6ca6119eee5f95a5bb53005f7f4325d38465e400
Ksenia Coulter <kcoulter@gmail.com>
2020-07-02 14:48:59 -0400

    add tests to pricePerUnit and unitSales

--- src/components/organisms/pricePerUnit/pricePerUnit.test.js
+++ src/components/organisms/pricePerUnit/pricePerUnit.test.js
@@ -2,22 +2,52 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import PricePerUnit from './pricePerUnit'
+import { CALCULATOR_STEPS } from '../../../constants/constants'
 
 describe('PricePerUnit', () => {
+  const setUnitPriceMock = jest.fn()
+  const goToStepMock = jest.fn()
+  const restartMock = jest.fn()
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('renders money input field and submit button', () => {
     const wrapper = shallow(<PricePerUnit />)
     expect(wrapper.find('MoneyInput')).toHaveLength(1)
     expect(wrapper.find('FormButton')).toHaveLength(1)
   })
 
-  it('calls setNumUnits prop and goes to step 4 on submit', () => {
-    const setUnitPriceMock = jest.fn()
-    const goToStepMock = jest.fn()
+  it('calls setUnitPrice on change', () => {
     const wrapper = shallow(
       <PricePerUnit setUnitPrice={setUnitPriceMock} goToStep={goToStepMock} />
     )
+    wrapper.find('MoneyInput').simulate('change', null, {value: 100})
+    expect(setUnitPriceMock).toHaveBeenCalledWith(100)
+  })  
+
+  it('goes to the next step on submit', () => {
+    const wrapper = shallow(
+      <PricePerUnit goToStep={goToStepMock} />
+    )
     wrapper.find('Form').simulate('submit')
-    expect(setUnitPriceMock).toHaveBeenCalledWith(0)
-    expect(goToStepMock).toHaveBeenCalledWith(4)
+    expect(goToStepMock).toHaveBeenCalledWith(CALCULATOR_STEPS.PRICE_PER_UNIT + 1)
+  })
+
+  it('goes to the previous step on back click', () => {
+    const wrapper = shallow(
+      <PricePerUnit goToStep={goToStepMock} />
+    )
+    wrapper.find('a').first().simulate('click')
+    expect(goToStepMock).toHaveBeenCalledWith(CALCULATOR_STEPS.PRICE_PER_UNIT - 1)
+  })
+
+  it('calls restart prop on restart analysis click', () => {
+    const wrapper = shallow(
+      <PricePerUnit restart={restartMock} />
+    )
+    wrapper.find('a').last().simulate('click')
+    expect(restartMock).toHaveBeenCalledTimes(1)
   })
 })
--- src/components/organisms/unitSales/unitSales.test.js
+++ src/components/organisms/unitSales/unitSales.test.js
@@ -2,22 +2,53 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import UnitSales from './unitSales'
+import { CALCULATOR_STEPS } from '../../../constants/constants'
+
 
 describe('UnitSales', () => {
+  const setNumUnitsMock = jest.fn()
+  const goToStepMock = jest.fn()
+  const restartMock = jest.fn()
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('renders an input field and submit button', () => {
     const wrapper = shallow(<UnitSales />)
     expect(wrapper.find('Input')).toHaveLength(1)
     expect(wrapper.find('FormButton')).toHaveLength(1)
   })
 
-  it('calls setNumUnits prop and goes to step 3 on submit', () => {
-    const setNumUnitsMock = jest.fn()
-    const goToStepMock = jest.fn()
+  it('calls setNumUnits on Change', () => {
     const wrapper = shallow(
-      <UnitSales setNumUnits={setNumUnitsMock} goToStep={goToStepMock} />
+      <UnitSales setNumUnits={setNumUnitsMock} />
+    )
+    wrapper.find('Input').simulate('change', null, {value: 100})
+    expect(setNumUnitsMock).toHaveBeenCalledWith(100)
+  })
+
+  it('goes to the next step on submit', () => {
+    const wrapper = shallow(
+      <UnitSales goToStep={goToStepMock} />
     )
     wrapper.find('Form').simulate('submit')
-    expect(setNumUnitsMock).toHaveBeenCalledWith(0)
-    expect(goToStepMock).toHaveBeenCalledWith(3)
+    expect(goToStepMock).toHaveBeenCalledWith(CALCULATOR_STEPS.UNIT_SALES + 1)
+  })
+
+  it('goes to the previous step on back click', () => {
+    const wrapper = shallow(
+      <UnitSales goToStep={goToStepMock} />
+    )
+    wrapper.find('a').first().simulate('click')
+    expect(goToStepMock).toHaveBeenCalledWith(CALCULATOR_STEPS.UNIT_SALES - 1)
+  })
+
+  it('calls restart prop on restart analysis click', () => {
+    const wrapper = shallow(
+      <UnitSales restart={restartMock} />
+    )
+    wrapper.find('a').last().simulate('click')
+    expect(restartMock).toHaveBeenCalledTimes(1)
   })
 })

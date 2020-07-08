3d86a78c052beddebbb4c856ceb01cd86f4b228e
Ksenia Coulter <kcoulter@gmail.com>
2020-07-08 15:15:51 -0400

    Start styling welcome card

--- src/components/molecules/breakEvenWelcomeCard/breakEvenWelcomeCard.js
+++ src/components/molecules/breakEvenWelcomeCard/breakEvenWelcomeCard.js
@@ -1,49 +1,61 @@
 import React from 'react'
 
-import { Container, Grid } from 'semantic-ui-react'
+import { Container, Grid, Image } from 'semantic-ui-react'
+
+import calculatorIcon from '../../../images/calculator_icon.svg'
+import './BreakEvenWelcomeCard.less'
 
 const BreakEvenWelcomeCard = (props) => {
-  const {expectedUnits, breakEvenUnits, pricePerUnit, variableCost} = props;
+  const { expectedUnits, breakEvenUnits, pricePerUnit, variableCost } = props;
 
   const willBreakEven = expectedUnits > breakEvenUnits;
   return(
-    <Container className='breakEvenProfile-container'>
-      <h3>Welcome</h3>
-      <h4>Your Break-Even Point Results</h4>
-      <p>Estimated calculation based on your inputs</p>
-      <Grid columns={2}>
-        <Grid.Column>
-          <div className='units'>
-            <div className='heading'>
-              Units
-              <div className='number'>100</div>
+    <Container className='breakEvenWelcome-container'>
+      <Grid>
+        <Grid.Row columns={2} stretched>
+          <Grid.Column width={4}>
+            <Image src={calculatorIcon} alt='calculator icon' size='tiny' centered/>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <h3>Welcome</h3>
+            <h4>Your Break-Even Point Results</h4>
+            <p>Estimated calculation based on your inputs</p>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={2} stretched>
+          <Grid.Column>
+            <div className='units'>
+              <div className='heading'>
+                Units
+                <div className='number'>100</div>
+                <p>needed to sell in order to cover your costs</p>
+              </div>
+              <p>If you sell your anticipated</p>
+              <div className='number'>{`${expectedUnits}`}</div>
+              <p>{`your ${willBreakEven ? 'profit' : 'loss'} will be`}</p>
+              <div className={`number ${willBreakEven ? 'profit' : 'loss'}`}>
+                {expectedUnits - breakEvenUnits * pricePerUnit}
+              </div>
             </div>
-            <p>needed to sell in order to cover your costs</p>
-            <p>If you sell your anticipated</p>
-            <div className='number'>{`${expectedUnits}`}</div>
-            <p>{`your ${willBreakEven ? 'profit' : 'loss'} will be`}</p>
-            <div className={`number ${willBreakEven ? 'profit' : 'loss'}`}>
-              {expectedUnits - breakEvenUnits * pricePerUnit}
+          </Grid.Column>
+          <Grid.Column>
+            <div className='sales'>
+              <div className='heading'>
+                Sales
+                <div className='number'>$1,000</div>
+                <p>revenue dollars needed to break even</p>
+              </div>
+              <p>Contribution margin ratio</p>
+              <div className='number'>
+                {(pricePerUnit - variableCost) / pricePerUnit}
+              </div>
+              {!willBreakEven && <p>You will need to sell 
+                <span className='number loss'> {123} </span>
+                more to break even
+              </p>}
             </div>
-          </div>
-        </Grid.Column>
-        <Grid.Column>
-          <div className='sales'>
-            <div className='heading'>
-              Sales
-              <div className='number'>$1,000</div>
-            </div>
-            <p>revenue dollars needed to break even</p>
-            <p>Contribution margin ratio</p>
-            <div className='number'>
-              {(pricePerUnit - variableCost) / pricePerUnit}
-            </div>
-            {!willBreakEven && <p>You will need to sell 
-              <span className='number loss'>{} </span>
-              more to break even
-            </p>}
-          </div>
-        </Grid.Column>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     </Container>
   )

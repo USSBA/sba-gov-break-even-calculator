c2f03a97fab4904d5b0ed88e8c129fd2a7e0d1bc
Ksenia Coulter <kcoulter@gmail.com>
2020-07-21 16:38:27 -0400

    Show different content based on whether edit mode is on

--- src/components/atoms/editableTotal/editableTotal.js
+++ src/components/atoms/editableTotal/editableTotal.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Grid } from 'semantic-ui-react'
+import { Grid, Button, Input } from 'semantic-ui-react'
+import { MoneyInput } from '../../atoms'
 
 const EditableTotal = (props) => {
   const {title, value, onEdit} = props;
+
+  const [fieldValue, setFieldValue] = useState(value)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const handleInputFieldChange = (value) => {
+    setIsEditing(true)
+    setFieldValue(value)
+  }
+
+  const handleApply = () => {
+    onEdit(fieldValue)
+    setIsEditing(false)
+  }
+
   return (
+    <>
     <Grid.Row columns={2}>
-      <Grid.Column floated={left} className='title'>{title}</Grid.Column>
-      <Grid.Column floated={right} className='editableSection'>
-        <div>{value}</div>
-        <a onClick={onEdit}>Edit</a>
+      <Grid.Column className='title'>{title}</Grid.Column>
+      <Grid.Column textAlign='right' className='editableSection'>
+        {isEditing ? 
+        (<Input 
+          label={{basic: true, content: '$'}} 
+          action={{
+            content:'APPLY',
+            onClick: () => handleApply(),
+          }}
+          type='number'
+          value={fieldValue} 
+          onChange={(e, { value }) => {handleInputFieldChange(value)}}
+        />) :
+        (<>
+          <span>{value}</span>
+          <a onClick={() => setIsEditing(true)}>Edit</a>
+        </>)
+        }
       </Grid.Column >
     </Grid.Row>
+    </>
   )
   
 }

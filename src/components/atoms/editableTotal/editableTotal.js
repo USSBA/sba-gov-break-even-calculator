5de5de34016937444feb9b8dba146b424e1e4456
Ksenia Coulter <kcoulter@gmail.com>
2020-07-22 16:10:01 -0400

    Hide and show running totals according to page. Handle outside click

--- src/components/atoms/editableTotal/editableTotal.js
+++ src/components/atoms/editableTotal/editableTotal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import { Grid, Input, Form } from 'semantic-ui-react'
 import { formatNumber } from '../../../helpers'
@@ -7,8 +7,26 @@ import './editableTotal.less'
 const EditableTotal = (props) => {
   const {title, value, type='currency', onEdit} = props;
 
+  const node = useRef()
+
   const [fieldValue, setFieldValue] = useState(value)
   const [isEditing, setIsEditing] = useState(false)
+
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClickOutside);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = e => {
+    if (node.current.contains(e.target)) {
+      return;
+    }
+    setIsEditing(false)
+  };
 
   const handleInputFieldChange = (value) => {
     setIsEditing(true)
@@ -25,31 +43,29 @@ const EditableTotal = (props) => {
     <Grid.Row className='editableTotal-row' columns={2}>
       <Grid.Column className='title'>{title}</Grid.Column>
       <Grid.Column textAlign='right' className='editableSection'>
-        {isEditing ? 
-        (
           <Form onSubmit={handleApply} size='small'>
             <Form.Field>
-              <Input 
-                label={{basic: true, content: `${type === 'currency' ? '$' : 'Units'}`}} 
-                action={{
-                  color: 'blue',
-                  content:'APPLY',
-                  onClick: () => handleApply(),
-                }}
-                type='number'
-                value={fieldValue} 
-                onChange={(e, { value }) => {handleInputFieldChange(value)}}
-              />
+              <div ref={node}>
+                {isEditing && <Input 
+                  label={{basic: true, content: `${type === 'currency' ? '$' : 'Units'}`}} 
+                  action={{
+                    color: 'blue',
+                    content:'APPLY',
+                    onClick: () => handleApply(),
+                  }}
+                  type='number'
+                  value={fieldValue} 
+                  onChange={(e, { value }) => {handleInputFieldChange(value)}}
+                />}
+              </div>
             </Form.Field>
           </Form>
-        ) :
-        (<>
+        {!isEditing && (<>
           <span className='editableValue'>
             {`${type === 'currency' ? '$' : ''}${formatNumber(value)} ${type !== 'currency' ? 'Units' : ''}`}
           </span>
           <a onClick={() => setIsEditing(true)}>edit</a>
-        </>)
-        }
+        </>)}
       </Grid.Column >
     </Grid.Row>
     </>

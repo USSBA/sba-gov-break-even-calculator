90c006e053f05dd3962e27539ac4569b99488d71
Ksenia Coulter <kcoulter@gmail.com>
2020-07-27 16:45:10 -0400

    Add math helpers

--- src/components/organisms/results/results.js
+++ src/components/organisms/results/results.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 import { Grid } from 'semantic-ui-react'
 import { BreakEvenProfileCard, BreakEvenResultsCard, BreakEvenGraph, BreakEvenDataTable } from '../../molecules'
+import { findStepSize } from '../../../helpers'
+
 import './results.less'
+
+const generateDataTableRow = (data) => {
+  const {units, pricePerUnit, variableCostPerUnit, fixedCost } = data
+
+  const revenue = Math.round(units * pricePerUnit);
+  const totalVariableCost = math.round(variableCostPerUnit * units);
+
+  return {
+    units: units,
+    profit: revenue - totalVariableCost - fixedCost,
+    revenue: revenue,
+    variableCost: totalVariableCost,
+    fixedCost: fixedCost,
+  }
+}
 
 const Results = (props) => {
   const { variableCostPerUnit, numUnits, pricePerUnit, totalFixedCost } = props
+
   const contributionMarginRatio = (pricePerUnit - variableCostPerUnit) / pricePerUnit;
   const breakEvenPointUnits = Math.round(totalFixedCost / (pricePerUnit - variableCostPerUnit))
   const breakEvenPointRevenue = Math.round(totalFixedCost / contributionMarginRatio)

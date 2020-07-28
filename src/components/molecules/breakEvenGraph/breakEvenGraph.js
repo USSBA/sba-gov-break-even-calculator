6138bc5a362fb62dec78d4edee35526c31d611c3
peggles2 <peggles2@users.noreply.github.com>
2020-07-28 15:47:10 -0400

    Feature/fixed costs graph (#22)
    * TC-255 - add fixed cost line

--- src/components/molecules/breakEvenGraph/breakEvenGraph.js
+++ src/components/molecules/breakEvenGraph/breakEvenGraph.js
@@ -35,7 +35,7 @@ const drawLineChart = (data) => {
     .x((d) => x(d.x))
     .y((d) =>y(d.y))
 
-  const paths = ['totalCost', 'breakEven']
+  const paths = ['totalCost', 'breakEven','fixedCost']
 
   // X - Axis
   g.append("g")
@@ -87,6 +87,9 @@ class BreakEvenGraph extends React.Component {
           </Label>
           <Label basic size='small'>
             <Icon className='totalCost' name='circle' />Total Costs
+          </Label>
+          <Label basic size='small'>
+            <Icon className='fixedCost' name='circle' />Fixed Costs
           </Label>
           <div>
             <svg id="lineChart"></svg>

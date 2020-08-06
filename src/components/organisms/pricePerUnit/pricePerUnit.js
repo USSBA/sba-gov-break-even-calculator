9c992c6fd7a81c12d49e75413362b9fc0878ad05
peggles2 <peggles2@users.noreply.github.com>
2020-08-06 18:38:47 -0400

    Feature/price per unit (#36)
    * TC-341 - Add validation to price per unit page

--- src/components/organisms/pricePerUnit/pricePerUnit.js
+++ src/components/organisms/pricePerUnit/pricePerUnit.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'semantic-ui-react'
 
 import { MoneyInput } from '../../atoms'
@@ -6,7 +6,18 @@ import { CALCULATOR_STEPS } from '../../../constants/constants'
 import './pricePerUnit.less'
 
 const PricePerUnit = (props) => {
+  const [formError, setFormError] = useState(false)
   const self = CALCULATOR_STEPS.PRICE_PER_UNIT
+
+  const handleSubmit = () => {  
+    if (!props.value && props.value !== 0) {
+      setFormError(true)
+    } else {
+      setFormError(false)
+      props.goToStep(self + 1)
+    }
+  }
+
   return (
     <div className={`pricePerUnit-container ${props.visible ? '' : 'hidden'}`}>
       <h3>Estimate your selling price per unit</h3>
@@ -15,14 +26,19 @@ const PricePerUnit = (props) => {
         <span className="subtext">* indicates required field</span>
       </p>
 
-      <Form onSubmit={() => props.goToStep(self + 1)}>
+      <Form onSubmit={handleSubmit}>
         <div>
           <label htmlFor='unit price'>Per unit selling price*</label>
           <p className="subtext">Enter the price you plan to sell per unit or service:</p>
           <MoneyInput
             autoFocus
             value={props.value}
-            onChange={(e, {value}) => props.setUnitPrice(value)}
+            formError= {formError}
+            errorMessage= 'Enter a valid price per unit to continue'
+            onChange={(e, { value }) => {
+              props.setUnitPrice(value)
+              setFormError(false)
+            }}
             name='unit price' />
         </div>
         <div className='button-container'>

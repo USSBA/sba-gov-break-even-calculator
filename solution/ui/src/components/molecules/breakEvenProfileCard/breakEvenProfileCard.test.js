5f0b3f875f6f72385e31fbc3df6a1186f803858a
Ksenia Coulter <kzherdeva@ucdavis.edu>
2020-08-27 13:01:57 -0400

    fix failing tests

--- solution/ui/src/components/molecules/breakEvenProfileCard/breakEvenProfileCard.test.js
+++ solution/ui/src/components/molecules/breakEvenProfileCard/breakEvenProfileCard.test.js
@@ -4,24 +4,19 @@ import { shallow } from 'enzyme'
 import BreakEvenProfileCard from './breakEvenProfileCard'
 
 describe('BreakEvenProfileCard', () => {
-  it('renders two circles', () => {
-    const wrapper = shallow(
-      <BreakEvenProfileCard 
-        breakEvenUnits={'100'}
-        breakEvenSales={'1,200'}
-      />
-    )
-    expect(wrapper.find('.circleContent')).toHaveLength(2)
-  })
+  const sampleProps = {
+    variableCostPerUnit: '10',
+    numUnits: '90',
+    pricePerUnit: '110',
+    totalFixedCost: '1000',
+    updateNumUnits: jest.fn(),
+    updatePricePerUnit: jest.fn(),
+    updateFixedCost: jest.fn(),
+    updateVariableCost: jest.fn()
+  }
 
-  it('passes the correct values to respective circles', () => {
-    const wrapper = shallow(
-      <BreakEvenProfileCard 
-        breakEvenUnits={'100'}
-        breakEvenSales={'1,200'}
-      />
-    )
-    expect(wrapper.find('.unitsCircle .number').text()).toEqual('100')
-    expect(wrapper.find('.salesCircle .number').text()).toEqual('$1,200')
+  it('renders running totals', () => {
+    const wrapper = shallow(<BreakEvenProfileCard {...sampleProps}/>)
+    expect(wrapper.find('EditableTotal')).toHaveLength(4)
   })
 })

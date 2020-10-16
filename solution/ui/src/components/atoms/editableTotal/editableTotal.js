e3974ed9023ada63502f0c6ca7a8a20ec6c9b5b1
Ksenia Coulter <kzherdeva@ucdavis.edu>
2020-10-16 16:36:49 -0400

    Remove duplicate className declaration (#85)
    Co-authored-by: Ksenia Coulter <kcoulter@gmail.com>

--- solution/ui/src/components/atoms/editableTotal/editableTotal.js
+++ solution/ui/src/components/atoms/editableTotal/editableTotal.js
@@ -92,7 +92,7 @@ const EditableTotal = (props) => {
           </Form>
         {!isEditing && (
           <Grid columns={2} stackable={false}>
-            <Grid.Column className='totalValue-container' computer={11} tablet={10} mobile={10} verticalAlign='middle' className='editableValue'>
+            <Grid.Column className='totalValue-container editableValue' computer={11} tablet={10} mobile={10} verticalAlign='middle'>
               {formatTotals(props.value)}
             </Grid.Column>
             <Grid.Column className='editTotal-container' computer={5} tablet={6} mobile={6} verticalAlign='middle'>

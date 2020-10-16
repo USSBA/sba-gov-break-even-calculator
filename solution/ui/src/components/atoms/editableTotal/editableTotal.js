8abbb9e81d1ea4299e0c0d65a1fbeed8c7cb5e00
Ksenia Coulter <kzherdeva@ucdavis.edu>
2020-10-16 16:36:36 -0400

    Move handleClickOutside into the useEffect hook, and pass dependencies in (#86)
    Co-authored-by: Ksenia Coulter <kcoulter@gmail.com>

--- solution/ui/src/components/atoms/editableTotal/editableTotal.js
+++ solution/ui/src/components/atoms/editableTotal/editableTotal.js
@@ -21,21 +21,21 @@ const EditableTotal = (props) => {
 
 
   useEffect(() => {
+    const handleClickOutside = e => {
+      if (node.current.contains(e.target) || formError) {
+        return;
+      }
+      setIsEditing(false)
+      setFormError(false)
+    };
     // add when mounted
     document.addEventListener("mousedown", handleClickOutside);
     // return function to be called when unmounted
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
+  }, [node, formError]);
 
-  const handleClickOutside = e => {
-    if (node.current.contains(e.target) || formError) {
-      return;
-    }
-    setIsEditing(false)
-    setFormError(false)
-  };
 
   const handleInputFieldChange = (value) => {
     setFieldValue(value)

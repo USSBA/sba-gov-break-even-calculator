0b471398a4cdd10697591e324625532e72496e5f
Ksenia Coulter <kcoulter@gmail.com>
2020-12-18 11:37:51 -0500

    Convert modal tests to react testing library

--- solution/ui/src/components/atoms/modal/modal.test.js
+++ solution/ui/src/components/atoms/modal/modal.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import { render, screen } from '@testing-library/react'
 
 import Modal from './modal'
 
@@ -10,21 +10,9 @@ describe('Modal', () => {
     onClose: jest.fn()
   }
 
-  it('renders the content', () => {
-    const wrapper = shallow(<Modal {...sampleProps} />)
-    expect(wrapper.find('p').text()).toBe(`<Icon />${sampleProps.content}`)
+  test('renders the content', () => {
+    render(<Modal {...sampleProps} />)
+    expect(screen.getByRole('alert')).toHaveTextContent(sampleProps.content)
   })
 
-  it('calls onClose on click', () => {
-    const wrapper = shallow(<Modal {...sampleProps} />)
-    wrapper.find('.closeModal').simulate('click', {preventDefault: jest.fn()})
-    expect(sampleProps.onClose).toHaveBeenCalledTimes(1)
-  })
-
-  it('changes hidden class based on open prop', () => {
-    const wrapper = shallow(<Modal {...sampleProps} />)
-    expect(wrapper.find('.hidden')).toHaveLength(0)
-    wrapper.setProps({open: false})
-    expect(wrapper.find('.hidden')).toHaveLength(1)
-  })
 })
--- solution/ui/src/components/molecules/breakEvenProfileCard/breakEvenProfileCard.test.js
+++ solution/ui/src/components/molecules/breakEvenProfileCard/breakEvenProfileCard.test.js
@@ -1,20 +1,51 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import BreakEvenProfileCard from './breakEvenProfileCard'
 
-describe('BreakEvenProfileCard', () => {
-  const sampleProps = {
-    variableCostPerUnit: '10',
-    numUnits: '90',
-    pricePerUnit: '110',
-    totalFixedCost: '1000',
-    updateNumUnits: jest.fn(),
-    updatePricePerUnit: jest.fn(),
-    updateFixedCost: jest.fn(),
-    updateVariableCost: jest.fn()
-  }
+const sampleProps = {
+  variableCostPerUnit: '10',
+  numUnits: '90',
+  pricePerUnit: '110',
+  totalFixedCost: '1000',
+  updateNumUnits: jest.fn(),
+  updatePricePerUnit: jest.fn(),
+  updateFixedCost: jest.fn(),
+  updateVariableCost: jest.fn()
+}
 
+
+describe('Confirmation modal functionality', () => {
+  test('it shows confirmation modal on value change', () => {
+    render(<BreakEvenProfileCard {...sampleProps}/>)
+    userEvent.click(screen.queryAllByRole('button')[0])
+    userEvent.click(screen.getByLabelText('apply'))
+    expect(screen.queryByRole('alert')).toBeInTheDocument()
+  })
+
+  test('it closes confirmation modal on x click', () => {
+    render(<BreakEvenProfileCard {...sampleProps}/>)
+    userEvent.click(screen.queryAllByRole('button')[0])
+    userEvent.click(screen.getByLabelText('apply'))
+    expect(screen.queryByRole('alert')).toBeInTheDocument()
+    userEvent.click(screen.getByLabelText('close modal'))
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+  })
+
+  test('it closes confirmation modal after a timeout', async() => {
+    render(<BreakEvenProfileCard {...sampleProps}/>)
+    userEvent.click(screen.queryAllByRole('button')[0])
+    userEvent.click(screen.getByLabelText('apply'))
+    expect(screen.queryByRole('alert')).toBeInTheDocument()
+    await waitFor(() => {
+      expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+    }, {timeout: 4000})
+  })
+})
+
+describe('BreakEvenProfileCard', () => {
   it('renders running totals', () => {
     const wrapper = shallow(<BreakEvenProfileCard {...sampleProps}/>)
     expect(wrapper.find('EditableTotal')).toHaveLength(4)

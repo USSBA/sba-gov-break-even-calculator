8b34e3a540e6246235b4dfad52e3674615cdbcbd
Ksenia Coulter <kcoulter@gmail.com>
2021-01-08 13:38:47 -0500

    Add RTL tests for accordion

--- solution/ui/src/components/molecules/accordion/accordion.test.js
+++ solution/ui/src/components/molecules/accordion/accordion.test.js
@@ -1,17 +1,30 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import { FAQ_CONTENT } from '../../../constants'
 import BecAccordion from './accordion'
 
 describe('Accordion', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<BecAccordion data={[]} />)
-    expect(wrapper.find('Accordion')).toHaveLength(1)
+  
+  test('renders correct number of items', () => {
+    render(<BecAccordion data={FAQ_CONTENT[0]} />)
+    expect(screen.getAllByTestId('accordion-item')).toHaveLength(FAQ_CONTENT[0].length)
   })
 
-  it('renders correct number of items', () => {
-    const wrapper = shallow(<BecAccordion data={FAQ_CONTENT[0]} />)
-    expect(wrapper.find('AccordionItem')).toHaveLength(FAQ_CONTENT[0].length)
+  test('clicking on accordion item shows FAQ answer', () => {
+    render(<BecAccordion data={FAQ_CONTENT[0]} />)
+    expect(screen.getByText('First month’s rent')).not.toBeVisible()
+    fireEvent.click(screen.getByText('How to calculate a fixed cost that is not paid monthly'))
+    expect(screen.getByText('First month’s rent')).toBeVisible()
+  })
+
+  test('clicking on a different item, closes the previously open item', () => {
+    render(<BecAccordion data={FAQ_CONTENT[0]} />)
+    const secondQuestion = FAQ_CONTENT[0][1].question
+    const secondQuestionAnswer = FAQ_CONTENT[0][1].answer.replace('<p>', '').replace('</p>', '').trim()
+    fireEvent.click(screen.getByText('How to calculate a fixed cost that is not paid monthly'))
+    fireEvent.click(screen.getByText(secondQuestion))
+    expect(screen.getByText('First month’s rent')).not.toBeVisible()
+    expect(screen.getByText(secondQuestionAnswer)).toBeVisible()
   })
 })

4680e6fc6726c1e98e32f5babede71f0b2032cf7
Ksenia Coulter <kzherdeva@ucdavis.edu>
2021-01-21 10:24:37 -0500

    Use new logo assets, add link to root (#134)
    Co-authored-by: Ksenia Coulter <kcoulter@gmail.com>

--- solution/ui/src/components/molecules/footer/footer.js
+++ solution/ui/src/components/molecules/footer/footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './footer.less'
-import logo from '../../../images/sba_400x400.jpg'
+import logo from '../../../images/sba_logo.svg'
 import { Image, Grid } from 'semantic-ui-react'
 
 const Footer = () => (
@@ -8,7 +8,7 @@ const Footer = () => (
     <Grid className='footer' columns={1}>
       <Grid.Column textAlign='center'>
         <div className='footer-divider'></div>
-        <Image src={logo} size='tiny' alt='Small Business Administration' centered />
+        <Image className='footer-logo' href='/' src={logo} size='tiny' alt='Small Business Administration' centered />
         U.S. Small Business Administration | 409 3rd St, SW. Washington DC 20416
       </Grid.Column>
     </Grid>

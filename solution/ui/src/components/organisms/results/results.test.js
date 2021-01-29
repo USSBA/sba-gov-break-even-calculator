084f20d209b3cf55ee390fde25d0ec694598fed2
Parissa <parissa.eggleston@gmail.com>
2021-01-29 13:12:54 -0500

    TC-691 - write rtl tests for the results component

--- solution/ui/src/components/organisms/results/results.test.js
+++ solution/ui/src/components/organisms/results/results.test.js
@@ -1,79 +1,67 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-
-import Results from './results'
+import { render, screen } from '@testing-library/react';
+import { Results } from '../../organisms'
 
 describe('Results', () => {
-  const sampleProps = {
-    variableCostPerUnit: '10',
-    numUnits: '90',
-    pricePerUnit: '110',
-    totalFixedCost: '1000',
-    updateNumUnits: jest.fn(),
-    updatePricePerUnit: jest.fn(),
-    updateFixedCost: jest.fn(),
-    updateVariableCost: jest.fn()
-  }
+  beforeEach(() => {
+    render(<Results 
+      variableCostPerUnit='12'
+      numUnits='200'
+      pricePerUnit='25'
+      totalFixedCost='1000'
+      updateFixedCost={jest.fn()}
+      updateNumUnits={jest.fn()}
+      updatePricePerUnit={jest.fn()}
+      updateVariableCost={jest.fn()} 
+    />)
+  });
 
-  it('renders without crashing', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    expect(wrapper).toHaveLength(1)
+  test('renders break even result card', () => {
+    expect(screen.getByRole('heading', {
+      name: /your break\-even point results/i
+    })).toBeInTheDocument()
   })
 
-  it('renders break even result card', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    expect(wrapper.find('BreakEvenResultsCard')).toHaveLength(1)
+  test('renders break even profile card', () => {
+    expect(screen.getByRole('heading', {
+      name: /break\-even profile/i
+    })).toBeInTheDocument()
   })
 
-  it('passes correct props to BreakEvenResultsCard', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    expect(wrapper.find('BreakEvenResultsCard').props()).toEqual({
-      breakEvenRevenue: 1100,
-      breakEvenUnits: 10,
-      expectedUnits: sampleProps.numUnits,
-      fixedCost: sampleProps.totalFixedCost,
-      pricePerUnit: sampleProps.pricePerUnit,
-      variableCost: sampleProps.variableCostPerUnit,
-    })
+  test('renders the graph', () => {
+    expect(screen.getByRole('heading', {
+      name: /break\-even point graph/i
+    })).toBeInTheDocument()
   })
 
-  it('renders break even profile card', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    expect(wrapper.find('BreakEvenProfileCard')).toHaveLength(1)
+  test('renders BreakEvenDataTable', () => {
+    expect(screen.getByRole('heading', {
+      name: /break\-even point unit sales/i
+    })).toBeInTheDocument()
   })
 
-  it('passes correct props to BreakEvenProfileCard', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    expect(wrapper.find('BreakEvenProfileCard').props()).toEqual({
-      numUnits: sampleProps.numUnits,
-      totalFixedCost: sampleProps.totalFixedCost,
-      pricePerUnit: sampleProps.pricePerUnit,
-      variableCostPerUnit: sampleProps.variableCostPerUnit,
-      updateNumUnits: expect.any(Function),
-      updatePricePerUnit: expect.any(Function),
-      updateFixedCost: expect.any(Function),
-      updateVariableCost: expect.any(Function),
-    })
+  test('renders print CTA', () => {
+    expect(screen.getByRole('button', {
+      name: /print results/i
+    })).toBeInTheDocument()
   })
+})
 
-  it('renders the graph', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    expect(wrapper.find('BreakEvenGraph')).toHaveLength(1)
-  })
+describe ('ResultsWithHighVC', () => { 
+  test('doesn\'t render the graph when variable cost is equal or higher than price per unit', () => {
+    render(<Results 
+      variableCostPerUnit='1200'
+      numUnits='200'
+      pricePerUnit='25'
+      totalFixedCost='1000'
+      updateFixedCost={jest.fn()}
+      updateNumUnits={jest.fn()}
+      updatePricePerUnit={jest.fn()}
+      updateVariableCost={jest.fn()} 
+    />)
 
-  it('doesn\'t render the graph when variable cost is equal or higher than price per unit', () => {
-    const wrapper = shallow(<Results {...sampleProps} variableCostPerUnit={110} />)
-    expect(wrapper.find('BreakEvenGraph')).toHaveLength(0)
-  })
-
-  it('renders BreakEvenDataTable', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    const bepDataTable = wrapper.find('BreakEvenDataTable').dive()
-    expect(bepDataTable.find('TableRow')).toHaveLength(9)
-  })
-
-  it('renders print CTA', () => {
-    const wrapper = shallow(<Results {...sampleProps}/>)
-    expect(wrapper.find('#printCTA')).toHaveLength(1)
+    expect(screen.queryByRole('heading', {
+      name: /break\-even point graph/i
+    })).not.toBeInTheDocument()
   })
 })

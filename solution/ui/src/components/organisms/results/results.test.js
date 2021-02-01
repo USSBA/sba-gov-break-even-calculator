4f5d6d41963080185f7fe6ad3b9c35591074daef
Parissa <parissa.eggleston@gmail.com>
2021-02-01 15:18:28 -0500

    add tests to ensure page updates when values are edited

--- solution/ui/src/components/organisms/results/results.test.js
+++ solution/ui/src/components/organisms/results/results.test.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react';
-import { Results } from '../../organisms'
+import { within, render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+
+import BreakEvenCalculator from '../../../pages/index'
 
 describe('Results', () => {
   beforeEach(() => {
-    render(<Results 
-      variableCostPerUnit='12'
-      numUnits='200'
-      pricePerUnit='25'
-      totalFixedCost='1000'
-      updateFixedCost={jest.fn()}
-      updateNumUnits={jest.fn()}
-      updatePricePerUnit={jest.fn()}
-      updateVariableCost={jest.fn()} 
-    />)
+    render(<BreakEvenCalculator />)
+    userEvent.click(screen.getByLabelText('yes, I know the total of my monthly fixed costs'))
+    userEvent.type(screen.getByLabelText('total fixed cost'), '1000')
+    fireEvent.submit(screen.getByTestId('fixedCosts-form'))
+    userEvent.type(screen.getByRole('spinbutton', { name: /per unit selling price\*/i }), '25')
+    userEvent.click(screen.getByRole('button', { name: /continue/i }))
+    userEvent.type(screen.getByRole('spinbutton', {  name: /number of units to sell\*/i}), '123')
+    userEvent.click(screen.getByRole('button', { name: /continue/i }))
+    userEvent.click(screen.getByRole('radio', {
+      name: /yes, i know the total of my variable costs per unit/i
+    }))
+    userEvent.type(screen.getByRole('spinbutton', { name: /total monthly variable costs\*/i }), '12')
+    userEvent.click(screen.getByRole('button', { name: /continue/i }))
+
   });
 
   test('renders break even result card', () => {
@@ -45,20 +51,179 @@ describe('Results', () => {
       name: /print results/i
     })).toBeInTheDocument()
   })
+
+  test('Changing number of units updates the results page values', () => {
+    //test number of units is currently at
+    expect(screen.getByText(/123 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/123/i)).toHaveLength(2)
+
+    //profit is currently at
+    expect(screen.getByText(/\$599/i)).toBeInTheDocument()
+
+    //unit sales value is currently
+    expect(screen.getByText(/\$1,923/i)).toBeInTheDocument()
+
+    //the breakeven value is currently at
+    const view = screen.getByTestId('breakevenLabel');
+    expect(within(view).getByText(/77/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/77/i)).toHaveLength(3)
+
+    // change number of units
+    userEvent.click(screen.getByRole('button', { name: /edit # of units/i }))
+    userEvent.clear(screen.getByRole('input', { name: /# of units/i }))
+    userEvent.type(screen.getByRole('input', { name: /# of units/i }), '555')
+    userEvent.click(screen.getByRole('button', { name: /apply/i }))
+
+    //profit is updated
+    expect(screen.getByText(/\$6,215/i)).toBeInTheDocument()
+
+    //unit sales value remains the same
+    expect(screen.getByText(/\$1,923/i)).toBeInTheDocument()
+
+    //number of units is updated everywhere.
+    expect(screen.getByText(/555 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/555/i)).toHaveLength(2)
+
+    //the breakeven value remains the same
+    const breakevenLabelView = screen.getByTestId('breakevenLabel');
+    expect(within(breakevenLabelView).getByText(/77/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/77/i)).toHaveLength(3)
+  })
+
+  test('Changing price per unit updates the results page values', () => {
+    //test number of units is currently at
+    expect(screen.getByText(/123 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/123/i)).toHaveLength(2)
+
+    //profit is currently at
+    expect(screen.getByText(/\$599/i)).toBeInTheDocument()
+
+    //unit sales value is currently
+    expect(screen.getByText(/\$1,923/i)).toBeInTheDocument()
+
+    //the breakeven value is currently at
+    const view = screen.getByTestId('breakevenLabel');
+    expect(within(view).getByText(/77/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/77/i)).toHaveLength(3)
+
+    // change number of units
+    userEvent.click(screen.getByRole('button', {  name: /edit price per unit/i}))
+    userEvent.clear(screen.getByRole('input', {  name: /price per unit/i}))
+    userEvent.type(screen.getByRole('input', {  name: /price per unit/i}), '55')
+    userEvent.click(screen.getByRole('button', { name: /apply/i }))
+
+    //profit is updated
+    expect(screen.getByText(/\$4,289/i)).toBeInTheDocument()
+
+    //unit sales value remains the same
+    expect(screen.getByText(/\$1,279/i)).toBeInTheDocument()
+
+    //number of units is updated everywhere.
+    expect(screen.getByText(/^123 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/^123/i)).toHaveLength(2)
+
+    //the breakeven value is updated
+    const breakevenLabelView = screen.getByTestId('breakevenLabel');
+    expect(within(breakevenLabelView).getByText(/^23/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/^23/i)).toHaveLength(3)
+  })
+
+
+  test('Changing fixed costs updates the results page values', () => {
+    //test number of units is currently at
+    expect(screen.getByText(/123 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/123/i)).toHaveLength(2)
+
+    //profit is currently at
+    expect(screen.getByText(/\$599/i)).toBeInTheDocument()
+
+    //unit sales value is currently
+    expect(screen.getByText(/\$1,923/i)).toBeInTheDocument()
+
+    //the breakeven value is currently at
+    const view = screen.getByTestId('breakevenLabel');
+    expect(within(view).getByText(/77/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/77/i)).toHaveLength(3)
+
+    // change number of units
+    userEvent.click(screen.getByRole('button', { name: /edit fixed costs/i }))
+    userEvent.clear(screen.getByRole('input', {  name: /fixed costs/i}))
+    userEvent.type(screen.getByRole('input', {  name: /fixed costs/i}), '1003')
+    userEvent.click(screen.getByRole('button', { name: /apply/i }))
+
+    //profit is updated
+    expect(screen.getByText(/\$596/i)).toBeInTheDocument()
+
+    //unit sales value is updated
+    expect(screen.getByText(/\$1,929/i)).toBeInTheDocument()
+
+    //number of units is updated everywhere.
+    expect(screen.getByText(/^123 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/^123/i)).toHaveLength(2)
+
+    //the breakeven value is updated
+    const breakevenLabelView = screen.getByTestId('breakevenLabel');
+    expect(within(breakevenLabelView).getByText(/^77/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/^77/i)).toHaveLength(3)
+  })
+
+  test('Changing variable costs updates the results page values', () => {
+    //test number of units is currently at
+    expect(screen.getByText(/123 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/123/i)).toHaveLength(2)
+
+    //profit is currently at
+    expect(screen.getByText(/\$599/i)).toBeInTheDocument()
+
+    //unit sales value is currently
+    expect(screen.getByText(/\$1,923/i)).toBeInTheDocument()
+
+    //the breakeven value is currently at
+    const view = screen.getByTestId('breakevenLabel');
+    expect(within(view).getByText(/77/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/77/i)).toHaveLength(3)
+
+    // change variable cost
+    userEvent.click(screen.getByRole('button', {  name: /edit variable costs/i}))
+    userEvent.clear(screen.getByRole('input', {  name: /variable costs/i}))
+    userEvent.type(screen.getByRole('input', {  name: /variable costs/i}), '8')
+    userEvent.click(screen.getByRole('button', { name: /apply/i }))
+
+    //profit is updated
+    expect(screen.getByText(/\$1,091/i)).toBeInTheDocument()
+
+    //unit sales value is updated
+    expect(screen.getByText(/\$1,471/i)).toBeInTheDocument()
+
+    //number of units is updated everywhere.
+    expect(screen.getByText(/^123 units/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/^123/i)).toHaveLength(2)
+
+    //the breakeven value is updated
+    const breakevenLabelView = screen.getByTestId('breakevenLabel');
+    expect(within(breakevenLabelView).getByText(/^59/i)).toBeInTheDocument()
+    expect(screen.getAllByText(/^59/i)).toHaveLength(3)
+  })
 })
+
 
 describe ('ResultsWithHighVC', () => { 
   test('doesn\'t render the graph when variable cost is equal or higher than price per unit', () => {
-    render(<Results 
-      variableCostPerUnit='1200'
-      numUnits='200'
-      pricePerUnit='25'
-      totalFixedCost='1000'
-      updateFixedCost={jest.fn()}
-      updateNumUnits={jest.fn()}
-      updatePricePerUnit={jest.fn()}
-      updateVariableCost={jest.fn()} 
-    />)
+    beforeEach(() => {
+      render(<BreakEvenCalculator />)
+      userEvent.click(screen.getByLabelText('yes, I know the total of my monthly fixed costs'))
+      userEvent.type(screen.getByLabelText('total fixed cost'), '1000')
+      fireEvent.submit(screen.getByTestId('fixedCosts-form'))
+      userEvent.type(screen.getByRole('spinbutton', { name: /per unit selling price\*/i }), '25')
+      userEvent.click(screen.getByRole('button', { name: /continue/i }))
+      userEvent.type(screen.getByRole('spinbutton', {  name: /number of units to sell\*/i}), '123')
+      userEvent.click(screen.getByRole('button', { name: /continue/i }))
+      userEvent.click(screen.getByRole('radio', {
+        name: /yes, i know the total of my variable costs per unit/i
+      }))
+      userEvent.type(screen.getByRole('spinbutton', { name: /total monthly variable costs\*/i }), '1200')
+      userEvent.click(screen.getByRole('button', { name: /continue/i }))
+    });
 
     expect(screen.queryByRole('heading', {
       name: /break\-even point graph/i

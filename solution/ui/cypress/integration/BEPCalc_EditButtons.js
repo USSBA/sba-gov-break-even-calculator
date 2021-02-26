175f14d835a95b9b2bf73f6d77995edd31daa2cf
mjackson21 <mjackson@fearless.tech>
2021-02-26 14:05:10 -0500

    add cypress tests for edit buttons

--- solution/ui/cypress/integration/BEPCalc_EditButtons.js
+++ solution/ui/cypress/integration/BEPCalc_EditButtons.js
@@ -1,7 +1,7 @@
 /// <reference types=“cypress” />
 
 describe ('BEP Calc Edit options', () => {
-    it('Clicks on the Fixed Costs edit link', () => {
+    it('Clicks on the Total fixed cost edit link', () => {
       cy.visit('localhost:8000')
       cy.get('.fixedCosts-container').scrollIntoView()
         .contains('Yes').click()
@@ -10,6 +10,31 @@ describe ('BEP Calc Edit options', () => {
       cy.get('#totalFixedCosts').type(5000)
       cy.get('[type="submit"]:visible').click()
       cy.get('.editButton').click()
+      cy.get('[aria-label="apply"]:visible').click()
     })
 
+    it('Clicks on the Selling price per unit edit link', () => {
+      cy.get('.pricePerUnit-container').scrollIntoView()
+      cy.get('#unitPrice').type(25)
+      cy.get('[type="submit"]:visible').click()
+      cy.get('[aria-label="edit Selling price per unit"]:visible').click()
+      cy.get('[aria-label="apply"]:visible').click()
+    })
+
+    it('Clicks on the Number of units edit link', () => {
+      cy.get('.unitSales-container').scrollIntoView()
+      cy.get('#units').type(2000)
+      cy.get('[type="submit"]:visible').click()
+      cy.get('[aria-label="edit Number of units"]:visible').click()
+      cy.get('[aria-label="apply"]:visible').click()
+    })
+
+    it('Clicks on the Variable Costs edit link', () => {
+      cy.get('.variableCosts-container').scrollIntoView()
+        .contains('Yes').click().click()
+      cy.get('#totalVariableCosts').type(10)
+      cy.get('[type="submit"]:visible').click()
+      cy.get('[aria-label="edit Variable Costs"]:visible').click()
+      cy.get('[aria-label="apply"]:visible').click()
+    })
 })
